d2485bd7d535fdf9967a71a8fa1b55882da61f78
Ali-mirzayi <alimirzaei7997@gmail.com>
2024-06-08 02:28:53 +0330

    fix uploads issuse v1

--- utils/DB.tsx
+++ utils/DB.tsx
@@ -10,7 +10,6 @@ export const createTable = () => {
   const db = getDBConnection();
   return db.transaction(tx => {
     tx.executeSql('CREATE TABLE IF NOT EXISTS rooms (id TEXT PRIMARY KEY NOT NULL, data TEXT NOT NULL)',
-    // tx.executeSql('CREATE TABLE IF NOT EXISTS rooms (id TEXT PRIMARY KEY NOT NULL,users TEXT NOT NULL, messages TEXT NOT NULL)',
     [],
     () => {console.log("table created")},
     (_, error):any => {console.log(error)}
@@ -20,8 +19,6 @@ export const createTable = () => {
 
 export const insertRoom = (room:Room) => {
   const db = getDBConnection();
-  // console.log(`INSERT OR REPLACE INTO rooms (data) VALUES ${JSON.stringify(room.id)}`,'query');
-  // console.log(`INSERT OR REPLACE INTO rooms (id, users, messages) VALUES ${room.id,room.users,room.messages}`,'query');
   return db.transaction(tx => {
     tx.executeSql(`INSERT OR IGNORE INTO rooms (id, data) VALUES (?, ?)`,
     [JSON.stringify(room.id), JSON.stringify(room)],
@@ -41,34 +38,6 @@ export const UpdateMessage = (data:Room) => {
     );
   });
 };
-
-// export const GetMessages = () => {
-//   const db = getDBConnection();
-//   return db.transaction(tx => {
-//     tx.executeSql(`SELECT * SET data = ? WHERE id = ?;`,)
-//   })
-// }
-
-
-// export const UpdateMessage = (data:Room) => {
-//   const db = getDBConnection();
-//   return new Promise((resolve, reject) => {
-//   db.transaction(tx => {
-//     tx.executeSql(`UPDATE rooms SET data = ? WHERE id = ?`,
-//     [JSON.stringify(data),JSON.stringify(data.id)],
-//     (_,{rows}) => {
-//       resolve(rows._array)
-//     },
-//     (_, error):any => reject(error)
-//     );
-//   });
-//     // @ts-ignore
-//     error => {
-//       console.log(error);
-//       reject(error);
-//     }
-//  })
-// };
 
 export const getAllRooms = () => {
   const db = getDBConnection();
